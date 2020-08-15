6a3b0f09e45bcdd769a642c047d6172ab48511bf
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-08-15 18:37:04 +0300

    some style

--- src/views/PostItem.vue
+++ src/views/PostItem.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="post-item" v-if="!loading">
-    <h1>{{post.title}}</h1>
-    <p>{{post.body}}</p>
+    <h1 class="post-item__title">{{post.title}}</h1>
+    <p class="post-item__text">{{post.body}}</p>
 
-    <div>
-      <h3>Comments</h3>
-      <div class="comments" v-for="comment in comments" :key="comment.id">
-        <ul>
-          <li>User: {{comment.email}}</li>
-          <li>Title: {{comment.name}}</li>
-          <li>Message: {{comment.body}}</li>
-        </ul>
+    <div class="comments">
+      <h3 class="comments__title">Comments:</h3>
+      <div class="comments-item" v-for="comment in comments" :key="comment.id">
+          <div class="comments-item__user">{{comment.email}}</div>
+          <div class="comments-item__title">{{comment.name}}</div>
+          <div class="comments-item__message">{{comment.body}}</div>
       </div>
     </div>
   </div>
@@ -47,5 +45,45 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.post-item {
+  
+  .post-item__title {
+    font-size: 35px;
+  }
 
+  .post-item__text {
+    margin: 0 0 40px;
+    font-size: 18px;
+  }
+}
+
+.comments {
+  .comments__title {
+    margin: 0 0 15px;
+  }
+
+  .comments-item {
+    margin: 0 0 10px;
+    border: 1px solid #ccc;
+    background-color: #e9ecef;
+    border-radius: 6px;
+    padding: 10px;
+
+    .comments-item__user {
+      font-size: 14px;
+      font-weight: bold;
+      margin: 0 0 20px;
+    }
+
+    .comments-item__title {
+      font-weight: bold;
+      font-size: 16px;
+      margin: 0 0 10px;
+    }
+
+    .comments-item__message {
+      font-size: 20px;
+    }
+  }
+}
 </style>
